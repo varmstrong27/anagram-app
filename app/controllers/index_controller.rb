f10db8e09e803617dd6a9298df53eedca60144cc
Veronica Armstrong <varmstrong27@gmail.com>
2015-11-24 13:52:34 -0800

    Updated to only return valid words

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,7 +1,8 @@
 get '/anagrams/:word' do
-	@word = params[:word]
-	@anagrams = Word.find_anagrams(@word)
-	erb :show
+    @word = params[:word]
+    alphabetized_string = @word.chars.sort.join
+    @anagrams = Word.where("letters=?", alphabetized_string)
+    erb :show
 end
 
 post '/' do
