95370a9d9161b131d41fb2355482dc1f21795fc3
Veronica Armstrong <varmstrong27@gmail.com>
2015-11-30 14:00:19 -0800

    Added page to create new word

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,3 +1,12 @@
+get '/words/new' do
+	erb :"/words/new"
+end
+
+post '/words' do
+	word = Word.create(text: params[:text])
+	redirect "/words/#{word.id}"
+end
+
 get '/words/:id' do
 	@word = Word.find(params[:id])
 	erb :"/words/show"
